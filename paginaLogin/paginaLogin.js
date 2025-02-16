5c183ebd19871190303d45d49984095439943c6e
Idilly Camily <idillycamily01@gmail.com>
2025-02-16 22:47:22 +0000

    Criação de Login e registro
    adicionei o login e o registro (no figma ta la pra adicionar foto mas na hora que eu criei, ficou estranho pra mim por isso nao adicionei) é isso

---
+++ paginaLogin/paginaLogin.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".container");
+const bntSignIn = document.getElementById("bnt-sign-in");
+const bntSignUp = document.getElementById("bnt-sign-up");
+
+bntSignIn.addEventListener("click", ()=>{
+    container.classList.remove("toggle");
+}); 
+
+bntSignUp.addEventListener("click", ()=>{
+    container.classList.add("toggle");
+});
+
+
+// icones de perfil de usuario
+
+const profilePic = document.getElementById("profilePic");
+const fileInput = document.getElementById("fileInput");
+
+profilePic.addEventListener("click", () => {
+    fileInput.click();
+});
+
+fileInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        profilePic.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+});
