be95298bef72445d37173c0b7a27912542f48b34
Igor Andrade <rogiandradee@gmail.com>
2025-04-09 20:52:04 +0100

    feat: :sparkles: update fastify config

--- Server/src/types/fastify.d.ts
+++ Server/src/types/fastify.d.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import 'fastify';
+import { PrismaClient } from '@prisma/client';
 
-declare module "fastify" {
+declare module 'fastify' {
   interface FastifyInstance {
     prisma: PrismaClient;
+  }
+
+  interface FastifyRequest {
+    user?: { userId: string };
   }
 }
