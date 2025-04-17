6711efcfb6ab720dacae1ece3b7e2edada342c79
Igor Andrade <rogiandradee@gmail.com>
2025-04-17 09:58:10 +0100

    feat: :sparkles: update route's importation

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,15 +1,36 @@
 import fastify from "fastify";
+import fastifyCookie from "fastify-cookie";
 import cors from "@fastify/cors";
+import dotenv from 'dotenv';
+
 import prismaPlugin from './lib/plugins/prisma';
 
 import { registerRoutes } from "./lib/routes/register";
+import { userRoutes } from "./lib/routes/user";
+import { authenticatedUserRoutes } from "./lib/routes/user/books";
+import { booksRoute } from "./lib/routes/books";
+import { adminBookRoutes } from "./lib/routes/admin/books";
+import { adminSupplierRoutes } from "./lib/routes/admin/suppliers";
+import { adminUserRoutes } from "./lib/routes/admin/users";
+import { logout } from "./lib/routes/logout/logout";
+
+dotenv.config();
 
 const app = fastify();
 
 app.register(prismaPlugin);
 
+app.register(fastifyCookie);
+
 app.register(cors)
 app.register(registerRoutes)
+app.register(userRoutes)
+app.register(authenticatedUserRoutes)
+app.register(booksRoute)
+app.register(adminBookRoutes)
+app.register(adminSupplierRoutes)
+app.register(adminUserRoutes)
+app.register(logout)
 
 app.listen({
     port: 3000,
